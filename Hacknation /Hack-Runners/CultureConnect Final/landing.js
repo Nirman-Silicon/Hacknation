76b34b87825214d540f7f02afc00c608c692349b
HackRunners <125138011+HackRunners@users.noreply.github.com>
2023-02-17 09:47:19 +0530

    front-end

---
+++ Hacknation /Hack-Runners/CultureConnect Final/landing.js
@@ -0,0 +1,24 @@
+createButton = document.querySelector("#create-button");
+postButton = document.querySelector("#postButton");
+crossButton = document.querySelector("#crossButton");
+
+myModal = document.querySelector("dialog.modal");
+// console.log(myModal);
+
+createButton.addEventListener("click", () => {
+  myModal.showModal();
+});
+
+postButton.addEventListener("click", () => {
+  myModal.close();
+});
+window.onclick = function (event) {
+  console.log(event.target);
+  if (event.target == myModal) {
+    myModal.close(); 
+  }
+};
+
+crossButton.addEventListener("click", () => {
+   myModal.close();
+});
